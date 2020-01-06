c5edb507b6ed715e7b6ceb1b4ec4fcc05127a5a0
briangeorge8 <56830933+briangeorge8@users.noreply.github.com>
2020-01-06 15:07:04 -0600

    Completed

--- 02_Variables/performance_labs/Perf_Lab_1_1_c_3.c
+++ 02_Variables/performance_labs/Perf_Lab_1_1_c_3.c
@@ -2,17 +2,24 @@
 
 int main(void)
 {
-	int <INT-VAR-NAME>;
-	float <FLOAT-VAR-NAME>;
-	double <DOUBLE-VAR-NAME>;
-	char <CHAR-VAR-NAME>;
-
+	// Declares and initializes the required variable types
+	int integer = 0;
+	float money = 11.23;
+	double result = 1.123456789012345;
+	char singleChar = 'A';
 	
-	printf("Your integer is %d \n", <INT-VAR-NAME>);
-	printf("Your float is %f \n", <FLOAT-VAR-NAME>);
-	printf("Your double is %lf \n", <DOUBLE-VAR-NAME>);
-	printf("Your first is %c \n", <CHAR-VAR-NAME>);
+	// Displays the saved content
+	printf("The integer's value is %d \n", integer);
+	printf("The float's value is %.2f \n", money);
+	printf("The double's value is %.15f \n", result);
+	printf("The char's value is %c \n", singleChar);
 
+	// Displays the size of each required data type
+	printf("The max size of int variables is %d byte(s).\n", sizeof(integer));
+	printf("The max size of float variables is %d byte(s).\n", sizeof(money));
+	printf("The max size of double variables is %d byte(s).\n", sizeof(result));
+	printf("The max size of char variables is %d byte(s).\n", sizeof(singleChar));
 	
+    // Ends main function & terminates program
 	return 0;
 }
